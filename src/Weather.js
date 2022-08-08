6dcc8b6951a664ef748584d3ce66af3cbe7f90ac
ASubby <104353408+ASubby@users.noreply.github.com>
2022-08-08 18:16:37 +0100

    imported axios

---
+++ src/Weather.js
@@ -0,0 +1,13 @@
+import React from "react";
+import axios from "axios";
+
+export default function Weather(props) {
+  function handleResponse(response) {
+    alert(`The weather in New York is ${response.data.main.temp}⁰C`);
+  }
+  let apiKey = "749717f0f4a2e36ed817dec27de38882";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(handleResponse);
+  return <h2>Hello Weather</h2>;
+}
